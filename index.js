b9fdf1e4f24f6a76c004999f6b2dd712951b0d6c
Jarae-MacyMan <sjaraebr@gmail.com>
2021-10-05 15:03:44 +0000

    1st question

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-//1
-function fizzBuzz(){
-  
+function fizzBuzz(num){ 
+  for(let i = 1; i < num+1; i++){
+    if (i % 5 === 0 && i % 3 === 0){
+      console.log("FizzBuzz");
+    }else if (i % 3 === 0){
+      console.log("fizz");
+    }else if (i % 5 === 0){
+      console.log("Buzz");
+    }else{
+  console.log(i);
+    }
+  }
 }
+
+fizzBuzz(100);
+
 
 //2
 function fizzBuzz(number){
